32c9acc1fab4f3ad4e94c01a9bd89b668c1fa65a
Childish <matthschmitz.mms@gmail.com>
2021-09-29 12:22:39 +0200

    authent

---
+++ controleur/controleurLog.php
@@ -0,0 +1,24 @@
+<?php
+require_once("../lib/authentification.php");
+
+
+
+if(!empty($_POST["login"]) && !empty($_POST["mdp"])){
+    $login = htmlspecialchars($_POST["login"]);
+    $mdp =  htmlspecialchars($_POST["mdp"]);
+
+    $authentification = new authentification();
+
+    if($authentification->login($login,$mdp)){
+        require_once ("../vue/vuePanel");
+    }
+    else{
+        Header("Location:../vue/vueConnexion.php");
+    }
+}
+else{
+    Header("Location:../vue/vueConnexion.php");
+}
+
+
+
