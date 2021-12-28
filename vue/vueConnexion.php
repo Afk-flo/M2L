7df1a17f301650b5279da20bf90cafc46b65530d
Flo <chauvain.florian@gmail.com>
2021-12-28 15:24:48 +0100

    bruteforce omega alpha 15

--- vue/vueConnexion.php
+++ vue/vueConnexion.php
@@ -1,16 +1,23 @@
-<div class="conteneur">
 	<header>
 		<?php include 'haut.php' ;?>
 	</header>
-	<main>
-		<div class="contentConnexion">
-			<div class='connexion'>
-				<div class='titre'>Veuillez vous identifier</div>
+	<main>			
+				<?php 
+			// Mise en place des messages flashs si besoin
+			if(isset($_SESSION['message']) && !empty($_SESSION['message'])) {
+				?>
+				<div class="<?php echo $_SESSION['message']['type']; ?>">
+					<span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
+					<strong>FLASH :</strong> <?php echo $_SESSION['message']['message']; ?>
+				</div>
+				<?php
+				$_SESSION['message'] = [];
+			}
+			?>
 				<?php	
 					$formulaireConnexion->afficherFormulaire();
 				?>
-			</div>
-		</div>
+
 	</main>
 	<footer>
 		<?php include 'bas.php' ;?>
